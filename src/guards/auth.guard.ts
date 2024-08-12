91c1066181fe9968edd90a7c4602ef59d0c41106
shshamim63 <shshamim63@gmail.com>
2024-08-12 23:09:43 +0600

    Refactored guard codes

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -4,19 +4,16 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-
-import * as jwt from 'jsonwebtoken';
-import { PrismaService } from '../prisma/prisma.service';
-import { JWTPayload } from '../interface/auth.interface';
 import { Request } from 'express';
-import { UserType } from '@prisma/client';
+
+import { User, UserType } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { TokenSerive } from '../token/token.service';
+
+import { ERROR_NAME, RESPONSE_MESSAGE, ROLES_KEY } from '../constants';
+import { RolesService } from '../roles/roles.service';
 import { Reflector } from '@nestjs/core';
-import {
-  AUTHORIZATION_TYPE,
-  ERROR_NAME,
-  RESPONSE_MESSAGE,
-  ROLES_KEY,
-} from '../constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -24,56 +21,44 @@ export class AuthGuard implements CanActivate {
 
   constructor(
     private readonly prismaService: PrismaService,
-    private reflector: Reflector,
+    private readonly tokenService: TokenSerive,
+    private readonly rolesService: RolesService,
+    private readonly reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride<UserType[]>(
-      ROLES_KEY,
-      [context.getHandler(), context.getClass()],
-    );
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.tokenService.getTokenFromHeader(request);
 
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+    if (!token) return true;
 
-    if (token) {
-      try {
-        const payload = (await jwt.verify(
-          token,
-          this.accessToken,
-        )) as JWTPayload;
+    const payload = this.tokenService.verifyToken(token);
+    let user: User;
+    try {
+      user = await this.prismaService.user.findUnique({
+        where: { id: payload.id },
+      });
 
-        const user = await this.prismaService.user.findUnique({
-          where: { id: payload.id },
-        });
+      if (!user)
+        throw new UnauthorizedException(
+          RESPONSE_MESSAGE.USER_MISSING,
+          ERROR_NAME.USER_MISSING,
+        );
 
-        if (user && requiredRoles?.length)
-          return requiredRoles.some((role) => user.userType?.includes(role));
+      const requiredRoles = this.getRoles(context);
 
-        if (user) return true;
+      if (!requiredRoles) return true;
 
-        return false;
-      } catch (error) {
-        let errorResponse: string;
-        let errorName: string;
-        if (error.name === ERROR_NAME.TOKEN_EXPIRED) {
-          errorResponse = RESPONSE_MESSAGE.TOKEN_EXPIRED;
-          errorName = ERROR_NAME.TOKEN_EXPIRED;
-        } else if (error.name === ERROR_NAME.INVALID_TOKEN) {
-          errorResponse = RESPONSE_MESSAGE.INVALID_TOKEN;
-          errorName = ERROR_NAME.INVALID_TOKEN;
-        } else {
-          return false;
-        }
-        throw new UnauthorizedException(errorResponse, errorName);
-      }
+      return this.rolesService.roleValidation(requiredRoles, user.userType);
+    } catch (error) {
+      throw error;
     }
-
-    return true;
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request?.headers?.authorization?.split(' ');
-    return type === AUTHORIZATION_TYPE ? token : undefined;
+  private getRoles(context: ExecutionContext): UserType[] | undefined {
+    return this.reflector.getAllAndOverride<UserType[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
   }
 }
