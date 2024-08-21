dcccd4f61696d4a7ed6e1b8c22c9eb9d658aa68e
shshamim63 <shshamim63@gmail.com>
2024-08-21 19:43:39 +0600

    Updated create expense functionalities

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,6 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ExpensesService } from './expenses.service';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { User } from '../decorators/user.decorator';
+import { ExpenseResponseDto } from './dto/expense.dto';
+import { CreateExpenseDto } from './dto/expense-create.dto';
+import { TaskInterceptor } from '../tasks/interceptors/task.interceptor';
+import { Task } from '../tasks/decorators/task.decorator';
 
 @Controller('tasks/:taskId/expenses')
+@UseGuards(AuthGuard)
+@UseInterceptors(TaskInterceptor)
 export class ExpensesController {
-  constructor() {}
+  constructor(private readonly expensesService: ExpensesService) {}
+
+  @Post()
+  createExpense(
+    @Body() createExpenseDto: CreateExpenseDto,
+    @User() user,
+    @Task() task,
+  ): Promise<ExpenseResponseDto> {
+    return this.expensesService.createExpense(user, task, createExpenseDto);
+  }
 }
--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JWTPayload } from '../interface/auth.interface';
+import { Task } from '@prisma/client';
+import { CreateExpenseDto } from './dto/expense-create.dto';
+import { ExpenseResponseDto } from './dto/expense.dto';
 
 @Injectable()
 export class ExpensesService {
-  constructor() {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createExpense(
+    user: JWTPayload,
+    task: Task,
+    createExpenseDto: CreateExpenseDto,
+  ) {
+    const data = {
+      ...createExpenseDto,
+      taskId: task.id,
+      contributorId: user.id,
+    };
+    try {
+      const expense = await this.prismaService.expense.create({ data });
+      return new ExpenseResponseDto(expense);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
