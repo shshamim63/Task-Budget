64b572aa0d334b9dc36d6fd7400e4867a15ec1f0
shshamim63 <shshamim63@gmail.com>
2024-08-27 21:46:50 +0600

    Updated the dto file name

--- src/collaborators/dto/create-collaborators.dto.ts
+++ src/collaborators/dto/create-collaborators.dto.ts
@@ -1,11 +1,11 @@
 import { Type } from 'class-transformer';
 import { ArrayMinSize, IsArray, IsInt, Min } from 'class-validator';
 
-export class CreateContributors {
+export class CreateCollaborators {
   @IsArray()
   @ArrayMinSize(1)
   @IsInt({ each: true })
   @Min(0, { each: true })
   @Type(() => Number)
-  contributors: number[];
+  collaborators: number[];
 }
