31080ccd2d1e301d6c612a3b6d28f06abb9e6ca7
shshamim63 <shshamim63@gmail.com>
2024-08-28 19:28:32 +0600

    Added functionalities for creating a new expense under a task

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { PrismaService } from '../prisma/prisma.service';
 import { JWTPayload } from '../interface/auth.interface';
@@ -6,6 +6,7 @@ import { JWTPayload } from '../interface/auth.interface';
 import { CreateExpenseDto } from './dto/expense-create.dto';
 import { ExpenseResponseDto } from './dto/expense.dto';
 import { TaskResponseDto } from '../tasks/dto/task.dto';
+import { UserType } from '@prisma/client';
 
 @Injectable()
 export class ExpensesService {
@@ -16,16 +17,41 @@ export class ExpensesService {
     task: TaskResponseDto,
     createExpenseDto: CreateExpenseDto,
   ) {
-    const data = {
-      ...createExpenseDto,
-      taskId: task.id,
-      contributorId: user.id,
-    };
     try {
+      const hasPermission =
+        user.userType === UserType.SUPER ||
+        (await this.isACollaborator(user.id, task.id));
+
+      if (!hasPermission)
+        throw new UnauthorizedException('User cannot initiate expense');
+
+      const data = {
+        ...createExpenseDto,
+        taskId: task.id,
+        contributorId: user.id,
+      };
+
       const expense = await this.prismaService.expense.create({ data });
+
       return new ExpenseResponseDto(expense);
     } catch (error) {
-      console.log(error);
+      throw error;
     }
+  }
+
+  private async isACollaborator(
+    userId: number,
+    taskId: number,
+  ): Promise<boolean> {
+    const collaboration = await this.prismaService.userTasks.findUnique({
+      where: {
+        memberId_taskId: {
+          memberId: userId,
+          taskId: taskId,
+        },
+      },
+    });
+
+    return collaboration ? true : false;
   }
 }
