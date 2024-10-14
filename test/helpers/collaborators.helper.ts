cc259c08a02ecc5c868a605d44ef018ae3716862
shshamim63 <shshamim63@gmail.com>
2024-10-14 22:03:46 +0600

    Added helper files for generating mock responses

---
+++ test/helpers/collaborators.helper.ts
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker/.';
+import { TaskStatus, UserType } from '@prisma/client';
+
+export const generateMockCollaboratorsResponse = () => {
+  return [
+    {
+      id: faker.number.int(),
+      title: faker.lorem.sentence(),
+      description: faker.lorem.paragraph(),
+      status: TaskStatus.OPEN,
+      budget: faker.number.int(),
+      creator: {
+        id: faker.number.int(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        userType: UserType.ADMIN,
+      },
+      members: [
+        {
+          id: faker.number.int(),
+          username: faker.internet.userName(),
+          email: faker.internet.email(),
+        },
+      ],
+    },
+  ];
+};
