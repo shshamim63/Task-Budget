70f397baa6e9a70dc7c436bbb3b579e6c6e6118a
shshamim63 <shshamim63@gmail.com>
2024-10-30 20:37:11 +0600

    Added test for the task-helper for ADMIN user

--- test/helpers/task-permission-helper.service.spec.ts
+++ test/helpers/task-permission-helper.service.spec.ts
@@ -5,6 +5,8 @@ import { generateUserJWTPayload } from '../test-seed/auth.helpers';
 import { TaskResponseDto } from '../../src/tasks/dto/task.dto';
 import { generateTask } from '../test-seed/task.helpers';
 import { UserType } from '@prisma/client';
+import { ForbiddenException } from '@nestjs/common';
+import { ERROR_NAME, RESPONSE_MESSAGE } from '../../src/utils/constants';
 
 describe('TaskPermissionService', () => {
   let service: TaskPermissionService;
@@ -22,6 +24,33 @@ describe('TaskPermissionService', () => {
     const task: TaskResponseDto = generateTask();
 
     const result = service.hasOperationPermission(user, task);
+
     expect(result).toBe(true);
+  });
+
+  it('should allow ADMIN user who created task to perform operation', () => {
+    const user: JWTPayload = generateUserJWTPayload(UserType.ADMIN);
+    const task: TaskResponseDto = generateTask();
+
+    const result = service.hasOperationPermission(user, {
+      ...task,
+      creatorId: user.id,
+    });
+
+    expect(result).toBe(true);
+  });
+
+  it('should not allow ADMIN user who is not the creator of the task by throwing ForbiddenException', () => {
+    const user: JWTPayload = generateUserJWTPayload(UserType.ADMIN);
+    const task: TaskResponseDto = generateTask();
+    expect(() => service.hasOperationPermission(user, task)).toThrow(
+      ForbiddenException,
+    );
+    expect(() => service.hasOperationPermission(user, task)).toThrow(
+      new ForbiddenException(
+        RESPONSE_MESSAGE.PERMISSION_DENIED,
+        ERROR_NAME.PERMISSION_DENIED,
+      ),
+    );
   });
 });
