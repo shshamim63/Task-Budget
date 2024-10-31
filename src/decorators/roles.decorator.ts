a00a6dd1786eef75cf5a2b50419cca8fe12dd70f
shshamim63 <shshamim63@gmail.com>
2024-10-31 19:31:59 +0600

    Added test for the Roles decorator

--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -1,6 +1,7 @@
 import { UserType } from '@prisma/client';
 
 import { SetMetadata } from '@nestjs/common';
+
 import { ROLES_KEY } from '../utils/constants';
 
 export const Roles = (...roles: UserType[]) => SetMetadata(ROLES_KEY, roles);
