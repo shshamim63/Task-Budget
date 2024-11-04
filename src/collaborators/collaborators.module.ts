796d0095d2950c5940d8466d8c6979120765d63f
shshamim63 <shshamim63@gmail.com>
2024-11-04 22:52:27 +0600

    Refactored collaborator code

--- src/collaborators/collaborators.module.ts
+++ src/collaborators/collaborators.module.ts
@@ -3,11 +3,22 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { TokenModule } from '../token/token.module';
 import { CollaboratorsController } from './collaborators.controller';
 import { CollaboratorsService } from './collaborators.service';
-import { TaskPermissionService } from '../helpers/task-permission-helper.service';
+import { TaskPermissionService } from '../helpers/task-permission.helper.service';
+import { CollaboratorRepository } from './repositories/collaborator.repository';
+import { TaskRepository } from '../tasks/repositories/task.repository';
+import { UserRepository } from '../auth/repository/user.repository';
+import { ErrorHandlerService } from '../helpers/error.helper.service';
 
 @Module({
   imports: [PrismaModule, TokenModule],
   controllers: [CollaboratorsController],
-  providers: [CollaboratorsService, TaskPermissionService],
+  providers: [
+    CollaboratorsService,
+    TaskPermissionService,
+    CollaboratorRepository,
+    TaskRepository,
+    UserRepository,
+    ErrorHandlerService,
+  ],
 })
 export class CollaboratorsModule {}
--- src/collaborators/collaborators.service.ts
+++ src/collaborators/collaborators.service.ts
@@ -1,23 +1,120 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
 import { JWTPayload } from '../auth/interfaces/auth.interface';
-import { PrismaService } from '../prisma/prisma.service';
+import { CreateCollaborator } from './interface/collaborator.interface';
+
 import { TaskResponseDto } from '../tasks/dto/task.dto';
 import { CreateCollaboratorsDto } from './dto/create-collaborators.dto';
-import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskCollaborators } from './dto/task-collaborators.dto';
-import { TaskPermissionService } from '../helpers/task-permission-helper.service';
+
+import { TaskPermissionService } from '../helpers/task-permission.helper.service';
+
+import { CollaboratorRepository } from './repositories/collaborator.repository';
+import { TaskRepository } from '../tasks/repositories/task.repository';
+import { UserRepository } from '../auth/repository/user.repository';
 
 @Injectable()
 export class CollaboratorsService {
   constructor(
-    private readonly prismaService: PrismaService,
+    private readonly collaboratorRepository: CollaboratorRepository,
+    private readonly taskRepository: TaskRepository,
+    private readonly userRepository: UserRepository,
     private readonly taskPermissionService: TaskPermissionService,
   ) {}
 
   async getCollaborators(user: JWTPayload, task: TaskResponseDto) {
     this.taskPermissionService.hasOperationPermission(user, task);
 
-    const taskWithMembers = await this.prismaService.task.findUnique({
+    const baseCondition = {
       where: { id: task.id },
+    };
+    const selector = this.generateGetCollaboratorSelector();
+
+    const query = { ...baseCondition, ...selector };
+
+    const taskCollaboratorInfo = await this.taskRepository.findUnique(query);
+
+    const taskWithCollaborators = {
+      ...taskCollaboratorInfo,
+      members: taskCollaboratorInfo.members.map((data) => data.member),
+    };
+
+    return new TaskCollaborators(taskWithCollaborators);
+  }
+
+  async assignMember(
+    createContributors: CreateCollaboratorsDto,
+    user: JWTPayload,
+    task: TaskResponseDto,
+  ): Promise<string> {
+    this.taskPermissionService.hasOperationPermission(user, task);
+
+    const { collaborators } = createContributors;
+
+    const whereClause = { where: { id: { in: collaborators } } };
+    const slelector = { select: { id: true } };
+
+    const existingCollaborators = await this.userRepository.findMany({
+      whereClause,
+      slelector,
+    });
+
+    this.validAssignableContributors(existingCollaborators, collaborators);
+
+    const currentTime = new Date();
+
+    const data: CreateCollaborator[] = collaborators.map((candidate) => {
+      return {
+        taskId: task.id,
+        memberId: candidate,
+        createdAt: currentTime,
+        updatedAt: currentTime,
+      };
+    });
+
+    await this.collaboratorRepository.createMany(data);
+
+    return `Assigned members to the task with id: ${task.id}`;
+  }
+
+  async removeCollaborator(
+    user: JWTPayload,
+    task: TaskResponseDto,
+    contributorId: number,
+  ): Promise<string> {
+    this.taskPermissionService.hasOperationPermission(user, task);
+    const query = {
+      where: {
+        memberId_taskId: {
+          memberId: contributorId,
+          taskId: task.id,
+        },
+      },
+    };
+
+    await this.collaboratorRepository.delete(query);
+
+    return `Removed member with id: ${contributorId} from task with id: ${task.id}`;
+  }
+
+  private validAssignableContributors(existingCollaborators, collaborators) {
+    const existingCollaboratorsIds = existingCollaborators.map(
+      (contributor) => contributor.id,
+    );
+
+    const missingContributors = collaborators.filter(
+      (id) => !existingCollaboratorsIds.includes(id),
+    );
+
+    if (missingContributors.length > 0) {
+      throw new NotFoundException(
+        `Contributor(s) with ID(s) ${missingContributors.join(', ')} not found`,
+      );
+    }
+  }
+
+  private generateGetCollaboratorSelector() {
+    return {
       select: {
         id: true,
         title: true,
@@ -46,76 +143,6 @@ export class CollaboratorsService {
           },
         },
       },
-    });
-
-    const taskWithCollaborators = {
-      ...taskWithMembers,
-      members: taskWithMembers.members.map((data) => data.member),
     };
-
-    return new TaskCollaborators(taskWithCollaborators);
-  }
-
-  async assignMember(
-    createContributors: CreateCollaboratorsDto,
-    user: JWTPayload,
-    task: TaskResponseDto,
-  ): Promise<string> {
-    this.taskPermissionService.hasOperationPermission(user, task);
-
-    const { collaborators } = createContributors;
-
-    const existingCollaborators = await this.prismaService.user.findMany({
-      where: { id: { in: collaborators } },
-      select: { id: true },
-    });
-
-    const existingCollaboratorsIds = existingCollaborators.map(
-      (contributor) => contributor.id,
-    );
-
-    const missingContributors = collaborators.filter(
-      (id) => !existingCollaboratorsIds.includes(id),
-    );
-
-    if (missingContributors.length > 0) {
-      throw new NotFoundException(
-        `Contributor(s) with ID(s) ${missingContributors.join(', ')} not found`,
-      );
-    }
-
-    const currentTime = new Date();
-
-    const data = collaborators.map((candidate) => {
-      return {
-        taskId: task.id,
-        memberId: candidate,
-        createdAt: currentTime,
-        updatedAt: currentTime,
-      };
-    });
-
-    await this.prismaService.userTasks.createMany({ data });
-
-    return `Assigned members to the task with id: ${task.id}`;
-  }
-
-  async removeCollaborator(
-    user: JWTPayload,
-    task: TaskResponseDto,
-    contributorId: number,
-  ): Promise<string> {
-    this.taskPermissionService.hasOperationPermission(user, task);
-
-    await this.prismaService.userTasks.delete({
-      where: {
-        memberId_taskId: {
-          memberId: contributorId,
-          taskId: task.id,
-        },
-      },
-    });
-
-    return `Removed member with id: ${contributorId} from task with id: ${task.id}`;
   }
 }
