3dfc919852f65cb6b5154d8608ecf632d334a2f6
shshamim63 <shshamim63@gmail.com>
2024-11-11 20:26:21 +0600

    Completed auth test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  BadRequestException,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 import * as bcrypt from 'bcrypt';
+import { faker } from '@faker-js/faker/.';
 
 import { AuthService } from './auth.service';
 import { TokenService } from '../token/token.service';
@@ -10,8 +16,8 @@ import { TokenServiceMock } from '../token/__mock__/token.service.mock';
 import { UserRepositoryMock } from './__mock__/user.repository.mock';
 import {
   generateMockEncryptedString,
+  mockSignInRequestBody,
   mockSignUpRequestBody,
-  mockTokenPayload,
   mockUser,
 } from './__mock__/auth-data.mock';
 
@@ -49,6 +55,7 @@ describe('AuthService', () => {
       const signUpCredentials = mockSignUpRequestBody();
       const newUser = mockUser(signUpCredentials);
       const token = generateMockEncryptedString();
+
       hashSpy.mockResolvedValueOnce(newUser.password_hash);
       UserRepositoryMock.findFirst.mockResolvedValueOnce(null);
       UserRepositoryMock.create.mockResolvedValueOnce(newUser);
@@ -68,6 +75,82 @@ describe('AuthService', () => {
         userType: newUser.userType,
       });
       expect(result).toMatchObject(newUser);
+    });
+    it('should throw an ConflictException when user with email exist', async () => {
+      const signUpCredentials = mockSignUpRequestBody();
+      const existingUser = mockUser({
+        ...signUpCredentials,
+        username: faker.internet.userName(),
+      });
+
+      UserRepositoryMock.findFirst.mockResolvedValueOnce(existingUser);
+
+      await expect(service.signup(signUpCredentials)).rejects.toThrow(
+        new ConflictException(
+          `Account with email ${signUpCredentials.email}` +
+            ' ' +
+            'already exist',
+        ),
+      );
+    });
+    it('should throw an ConflictException when user with username exist', async () => {
+      const signUpCredentials = mockSignUpRequestBody();
+      const existingUser = mockUser({
+        ...signUpCredentials,
+        email: faker.internet.email(),
+      });
+
+      UserRepositoryMock.findFirst.mockResolvedValueOnce(existingUser);
+
+      await expect(service.signup(signUpCredentials)).rejects.toThrow(
+        new ConflictException(
+          `Account with username ${signUpCredentials.username}` +
+            ' ' +
+            'already exist',
+        ),
+      );
+    });
+  });
+
+  describe('signin', () => {
+    it('should signin a user and return user response', async () => {
+      const singinCredentials = mockSignInRequestBody();
+      const currentUser = mockUser(singinCredentials);
+      const token = generateMockEncryptedString();
+
+      UserRepositoryMock.findUnique.mockResolvedValueOnce(currentUser);
+      compareSpy.mockResolvedValueOnce(true);
+      TokenServiceMock.generateToken.mockResolvedValueOnce(token);
+
+      const result = await service.signin(singinCredentials);
+
+      expect(result).toMatchObject(currentUser);
+      expect(tokenService.generateToken).toHaveBeenCalledWith({
+        id: currentUser.id,
+        email: singinCredentials.email,
+        username: currentUser.username,
+        userType: currentUser.userType,
+      });
+    });
+    it('should raise BadRequestException when used with email does not exist', async () => {
+      const singinCredentials = mockSignInRequestBody();
+
+      UserRepositoryMock.findUnique.mockResolvedValueOnce(null);
+
+      await expect(service.signin(singinCredentials)).rejects.toThrow(
+        new BadRequestException('Invalid credentials'),
+      );
+    });
+    it('should raise UnauthorizedException when password is invalid', async () => {
+      const singinCredentials = mockSignInRequestBody();
+      const currentUser = mockUser(singinCredentials);
+
+      UserRepositoryMock.findUnique.mockResolvedValueOnce(currentUser);
+      compareSpy.mockResolvedValueOnce(false);
+
+      await expect(service.signin(singinCredentials)).rejects.toThrow(
+        new UnauthorizedException('Invalid credentials'),
+      );
     });
   });
 });
