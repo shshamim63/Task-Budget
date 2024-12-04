04ac9035210a56259528f8a4f29e7f0897580b0a
shshamim63 <shshamim63@gmail.com>
2024-12-04 20:18:02 +0600

    Added mock for the helpers and prisma service

---
+++ src/helpers/__mock__/execute-with-error.helper.service.spec.ts
@@ -0,0 +1,3 @@
+export const AsyncErrorHandlerServiceMock = {
+  execute: jest.fn((callback) => callback()),
+};
---
+++ src/prisma/__mock__/prisma.service.mock.ts
@@ -0,0 +1,5 @@
+export const PrismaServiceMock = {
+  associate: {
+    create: jest.fn(),
+  },
+};
