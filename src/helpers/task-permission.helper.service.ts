c0c2c8c3824c9e69bf331e96f8f3a0209411aba3
shshamim63 <shshamim63@gmail.com>
2024-12-05 21:03:50 +0600

    Added task creation permission method

--- src/helpers/task-permission.helper.service.ts
+++ src/helpers/task-permission.helper.service.ts
@@ -1,11 +1,33 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { JWTPayload } from '../auth/interfaces/auth.interface';
+import { AuthUser, JWTPayload } from '../auth/interfaces/auth.interface';
 import { Task, UserType } from '@prisma/client';
 import { ERROR_NAME, RESPONSE_MESSAGE } from '../utils/constants';
 import { TaskResponseDto } from '../tasks/dto/task.dto';
 
 @Injectable()
 export class TaskPermissionService {
+  hasTaskCreationPermission(
+    currentUser: AuthUser,
+    enterpriseId: number,
+  ): boolean {
+    const isSuperUser = currentUser.userType === UserType.SUPER;
+
+    if (isSuperUser) return true;
+
+    const userCompanions = currentUser.companionOf.map(
+      (companion) => companion.id,
+    );
+
+    const adminIsACompanion = userCompanions.includes(enterpriseId);
+
+    if (adminIsACompanion) return true;
+
+    throw new ForbiddenException(
+      RESPONSE_MESSAGE.PERMISSION_DENIED,
+      ERROR_NAME.PERMISSION_DENIED,
+    );
+  }
+
   hasOperationPermission(
     user: JWTPayload,
     task: Task | TaskResponseDto,
