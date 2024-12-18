458521ad8957e2c5e32eae2e0c4acdd2c841ac78
shshamim63 <shshamim63@gmail.com>
2024-12-18 22:30:15 +0600

    Added redis module and service file

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
+  imports: [
+    NestRedisModule.forRoot({
+      type: 'single',
+      url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`,
+    }),
+  ],
   providers: [RedisService],
+  exports: [NestRedisModule],
 })
 export class RedisModule {}
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,30 @@
+import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Injectable } from '@nestjs/common';
 
+import Redis from 'ioredis';
+
 @Injectable()
-export class RedisService {}
+export class RedisService {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async set(key: string, value: string, ttl?: number): Promise<void> {
+    if (ttl) {
+      await this.redis.set(key, value, 'EX', ttl);
+    } else {
+      await this.redis.set(key, value);
+    }
+  }
+
+  async get(key: string): Promise<string | null> {
+    return await this.redis.get(key);
+  }
+
+  async del(key: string): Promise<number> {
+    return await this.redis.del(key);
+  }
+
+  async exists(key: string): Promise<boolean> {
+    const result = await this.redis.exists(key);
+    return result === 1;
+  }
+}
