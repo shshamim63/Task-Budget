ec08382a0a7004a8feca7167395a47d99f5fb47d
shshamim63 <shshamim63@gmail.com>
2024-12-23 22:23:25 +0600

    Updated redis file to fix the extra import

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
-import { RedisService } from './redis.service';
+import { Global, Module } from '@nestjs/common';
 import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
 
+import { RedisService } from './redis.service';
+
+import { AsyncErrorHandlerService } from '../helpers/execute-with-error.helper.service';
+import { ErrorHandlerService } from '../helpers/error.helper.service';
+
+@Global()
 @Module({
   imports: [
     NestRedisModule.forRoot({
@@ -9,7 +14,6 @@ import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
       url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`,
     }),
   ],
-  providers: [RedisService],
-  exports: [NestRedisModule],
+  providers: [RedisService, AsyncErrorHandlerService, ErrorHandlerService],
 })
 export class RedisModule {}
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,34 +1,36 @@
 import { InjectRedis } from '@nestjs-modules/ioredis';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 import Redis from 'ioredis';
+import { AsyncErrorHandlerService } from '../helpers/execute-with-error.helper.service';
 
 @Injectable()
 export class RedisService {
-  constructor(@InjectRedis() private readonly redis: Redis) {}
+  constructor(
+    @InjectRedis() private readonly redis: Redis,
+    private readonly asyncErrorHandlerService: AsyncErrorHandlerService,
+  ) {}
 
   async set(key: string, value: string, ttl?: number): Promise<void> {
-    try {
-      if (ttl) {
-        await this.redis.set(key, value, 'EX', ttl);
-      } else {
-        await this.redis.set(key, value);
-      }
-    } catch (error) {
-      throw new InternalServerErrorException('Failed to connect redis server');
-    }
+    const redisSetFunction = ttl
+      ? this.redis.set(key, value, 'EX', ttl)
+      : this.redis.set(key, value);
+    this.asyncErrorHandlerService.execute(() => redisSetFunction);
   }
 
   async get(key: string): Promise<string | null> {
-    return await this.redis.get(key);
+    return this.asyncErrorHandlerService.execute(() => this.redis.get(key));
   }
 
   async del(key: string): Promise<number> {
-    return await this.redis.del(key);
+    return this.asyncErrorHandlerService.execute(() => this.redis.del(key));
   }
 
   async exists(key: string): Promise<boolean> {
-    const result = await this.redis.exists(key);
+    const result = await this.asyncErrorHandlerService.execute(() =>
+      this.redis.exists(key),
+    );
+
     return result === 1;
   }
 }
