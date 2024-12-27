51ef5e1d0eb8e32e7995ab4c68e20318e77c2613
shshamim63 <shshamim63@gmail.com>
2024-12-27 20:05:43 +0600

    Configured redis store as cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
+
+import * as redisStore from 'cache-manager-redis-store';
+
 import { TaskModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ExpenseModule } from './expenses/expenses.module';
@@ -12,15 +16,22 @@ import { EnterpriseModule } from './enterprises/enterprises.module';
 import { AssociateModule } from './associates/associates.module';
 import { RedisModule } from './redis/redis.module';
 
+import { PrismaModule } from './prisma/prisma.module';
+import { TokenModule } from './token/token.module';
+
 import { AppController } from './app.controller';
 
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { TokenModule } from './token/token.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule.register({
+      host: `${process.env.REDIS_HOST || 'localhost'}`,
+      port: `${process.env.REDIS_PORT || 6379}`,
+      store: redisStore as unknown as CacheStore,
+      ttl: 3 * 6000,
+    }),
     TaskModule,
     AuthModule,
     ExpenseModule,
