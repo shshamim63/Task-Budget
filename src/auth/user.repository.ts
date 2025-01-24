e3352c618ead7052b90a21a8e8d7986d355f259e
shshamim63 <shshamim63@gmail.com>
2025-01-24 21:19:44 +0600

    resolved promise in the repository

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -11,25 +11,25 @@ export class UserRepository {
   ) {}
 
   async findFirst(query: Prisma.UserFindFirstArgs) {
-    return this.asyncErrorHandlerService.execute(() =>
+    return await this.asyncErrorHandlerService.execute(() =>
       this.prismaService.user.findFirst(query),
     );
   }
 
   async findUnique(query: Prisma.UserFindUniqueArgs) {
-    return this.asyncErrorHandlerService.execute(() =>
+    return await this.asyncErrorHandlerService.execute(() =>
       this.prismaService.user.findUnique(query),
     );
   }
 
   async findMany(query: Prisma.UserFindManyArgs) {
-    return this.asyncErrorHandlerService.execute(() =>
+    return await this.asyncErrorHandlerService.execute(() =>
       this.prismaService.user.findMany(query),
     );
   }
 
   async create(data) {
-    return this.asyncErrorHandlerService.execute(() =>
+    return await this.asyncErrorHandlerService.execute(() =>
       this.prismaService.user.create({ data }),
     );
   }
