b9bc310b89e8d4a04afb8fb8edeec6b532492157
shshamim63 <shshamim63@gmail.com>
2025-02-22 22:34:48 +0600

    Added refresh token route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto, SignUpDto } from './dto/auth-credentials.dto';
 
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -34,5 +34,18 @@ export class AuthController {
     });
 
     res.status(200).json(loginInfo);
+  }
+
+  @Post('/refresh')
+  async refreshToken(@Req() request: Request, @Res() res: Response) {
+    const refresTokenInfo = await this.authService.refreshToken(request);
+
+    res.cookie('refreshToken', refresTokenInfo.refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json(refresTokenInfo);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,8 @@ import {
 
 import * as bcrypt from 'bcrypt';
 
+import { Request } from 'express';
+
 import { TokenService } from '../token/token.service';
 
 import { UserResponseDto } from './dto/user.dto';
@@ -15,12 +17,13 @@ import {
   AuthUser,
   SignInParams,
   SignUpParams,
+  TokenPayload,
   TokenType,
 } from './interfaces/auth.interface';
 
 import { UserRepository } from '../users/user.repository';
 import { RedisService } from '../redis/redis.service';
-import { TOKENS } from '../utils/constants';
+import { ERROR_NAME, RESPONSE_MESSAGE, TOKENS } from '../utils/constants';
 
 @Injectable()
 export class AuthService {
@@ -69,7 +72,7 @@ export class AuthService {
 
     const payload = this.tokenService.createAuthTokenPayload({ ...user });
 
-    return this.generateUserResponse(user, payload);
+    return await this.generateUserResponse(user, payload, true);
   }
 
   async signin({ email, password }: SignInParams): Promise<UserResponseDto> {
@@ -98,27 +101,79 @@ export class AuthService {
 
     const payload = this.tokenService.createAuthTokenPayload({ ...user });
 
-    return this.generateUserResponse(user, payload);
+    return await this.generateUserResponse(user, payload, true);
   }
 
-  private async generateUserResponse(user, payload) {
+  async refreshToken(request: Request): Promise<UserResponseDto> {
+    const currentRefreshToken = this.tokenService.getTokenFromHeader(request);
+
+    if (!currentRefreshToken)
+      throw new UnauthorizedException(
+        RESPONSE_MESSAGE.INVALID_TOKEN,
+        ERROR_NAME.INVALID_TOKEN,
+      );
+
+    const { email } = this.tokenService.verifyToken(
+      currentRefreshToken,
+      TokenType.RefreshToken,
+    );
+
+    const findQuery = {
+      where: { email },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        userType: true,
+      },
+    };
+
+    const user = (await this.userRepository.findUnique(
+      findQuery,
+    )) as unknown as AuthUser;
+
+    if (!user) throw new BadRequestException('Invalid Request of the user');
+
+    const payload = this.tokenService.createAuthTokenPayload({ ...user });
+
+    const userInfoWithTokens = await this.generateUserResponse(
+      user,
+      payload,
+      false,
+    );
+
+    return { ...userInfoWithTokens, refreshToken: currentRefreshToken };
+  }
+
+  private async generateUserResponse(
+    user: AuthUser,
+    payload: TokenPayload,
+    newInstance: boolean,
+  ) {
     const accessToken = this.tokenService.generateToken(
       payload,
       TokenType.AccessToken,
     );
+    let refreshToken: string;
 
-    const refreshToken = this.tokenService.generateToken(
-      payload,
-      TokenType.RefreshToken,
-    );
-    const { duration } = TOKENS[TokenType.RefreshToken];
+    if (newInstance) {
+      refreshToken = this.tokenService.generateToken(
+        payload,
+        TokenType.RefreshToken,
+      );
+      const { duration } = TOKENS[TokenType.RefreshToken];
 
-    await this.redisService.set(
-      `token-user-${user.id}`,
-      refreshToken,
-      duration,
-    );
+      await this.redisService.set(
+        `token-user-${user.id}`,
+        refreshToken,
+        duration,
+      );
+    }
 
-    return new UserResponseDto({ ...user, accessToken, refreshToken });
+    return new UserResponseDto({
+      ...user,
+      accessToken,
+      ...(newInstance ? { refreshToken } : {}),
+    });
   }
 }
