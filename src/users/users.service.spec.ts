a8b98a5340efffd6ac7644fac37f49c62b60d6ae
shshamim63 <shshamim63@gmail.com>
2025-04-01 19:39:13 +0600

    Added test for the user password update service method

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ForbiddenException, UnauthorizedException } from '@nestjs/common';
+import { faker } from '@faker-js/faker/.';
+import * as bcrypt from 'bcrypt';
 
+import { UsersService } from './users.service';
 import { UserRepository } from './user.repository';
+import { RESPONSE_MESSAGE, USER_RESPONSE_MESSAGE } from '../utils/constants';
+
 import { UserRepositoryMock } from './__mock__/user.repository.mock';
 import { mockUser } from '../auth/__mock__/auth-data.mock';
 import { mockTokenPayload } from '../token/__mock__/token-data.mock';
-import { UsersService } from './users.service';
-import { UpdateUserPayloadMock } from './__mock__/user-data.mock';
-import { faker } from '@faker-js/faker/.';
-import { RESPONSE_MESSAGE } from '../utils/constants';
-import { ForbiddenException } from '@nestjs/common';
+import {
+  UpdateUserPasswordPayloadMock,
+  UpdateUserPayloadMock,
+} from './__mock__/user-data.mock';
 
 describe('UsersService', () => {
   let service: UsersService;
   let userRepository: UserRepository;
+  let compareSpy: jest.SpyInstance;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,6 +30,7 @@ describe('UsersService', () => {
 
     userRepository = module.get<UserRepository>(UserRepository);
     service = module.get<UsersService>(UsersService);
+    compareSpy = jest.spyOn(bcrypt, 'compare');
   });
 
   describe('getProfile', () => {
@@ -73,6 +80,75 @@ describe('UsersService', () => {
       ).rejects.toThrow(
         new ForbiddenException(RESPONSE_MESSAGE.PERMISSION_DENIED),
       );
+    });
+  });
+
+  describe('updateUserPassword', () => {
+    it('should throw ForbiddenException when current user id and updated user id is not same', async () => {
+      const invalidUserId = faker.number.int();
+      const currentUser = mockUser();
+      const currentUserPayload = mockTokenPayload(currentUser);
+      const upadtePasswordRequestBody = UpdateUserPasswordPayloadMock();
+
+      await expect(
+        service.updateUserPassword(
+          invalidUserId,
+          currentUserPayload,
+          upadtePasswordRequestBody,
+        ),
+      ).rejects.toThrow(
+        new ForbiddenException(RESPONSE_MESSAGE.PERMISSION_DENIED),
+      );
+    });
+    it('should throw ForbiddenException when current user is missing in the database', async () => {
+      const currentUser = mockUser();
+      const currentUserPayload = mockTokenPayload(currentUser);
+      const upadtePasswordRequestBody = UpdateUserPasswordPayloadMock();
+
+      UserRepositoryMock.findUnique.mockResolvedValue(false);
+
+      await expect(
+        service.updateUserPassword(
+          currentUserPayload.id,
+          currentUserPayload,
+          upadtePasswordRequestBody,
+        ),
+      ).rejects.toThrow(
+        new ForbiddenException(RESPONSE_MESSAGE.PERMISSION_DENIED),
+      );
+    });
+    it('should throw UnauthorizedException when currentPassword is invalid', async () => {
+      const currentUser = mockUser();
+      const currentUserPayload = mockTokenPayload(currentUser);
+      const upadtePasswordRequestBody = UpdateUserPasswordPayloadMock();
+
+      UserRepositoryMock.findUnique.mockResolvedValue(currentUser);
+      compareSpy.mockResolvedValue(false);
+
+      await expect(
+        service.updateUserPassword(
+          currentUserPayload.id,
+          currentUserPayload,
+          upadtePasswordRequestBody,
+        ),
+      ).rejects.toThrow(new UnauthorizedException('Invalid credentials'));
+    });
+    it('should return success response when all the data is valid', async () => {
+      const currentUser = mockUser();
+      const currentUserPayload = mockTokenPayload(currentUser);
+      const upadtePasswordRequestBody = UpdateUserPasswordPayloadMock();
+
+      UserRepositoryMock.findUnique.mockResolvedValue(currentUser);
+      compareSpy.mockResolvedValue(true);
+      UserRepositoryMock.update.mockResolvedValue(true);
+
+      const result = await service.updateUserPassword(
+        currentUserPayload.id,
+        currentUserPayload,
+        upadtePasswordRequestBody,
+      );
+
+      expect(result).toEqual(USER_RESPONSE_MESSAGE.UPDATE_PASSWORD);
     });
   });
 });
