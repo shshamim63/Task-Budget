a3eac8e6e9e0b8947aec5c1200fc75819dd45692
shshamim63 <shshamim63@gmail.com>
2025-05-19 21:29:21 +0600

    Fixed issued with the seed file and updated the unused variale eslint issues

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,86 +10,98 @@ const prisma = new PrismaClient();
 
 async function main() {
   // Create users
-  const adminUser = await prisma.user.create({
-    data: {
-      email: 'admin@example.com',
-      username: 'adminUser',
-      password_hash: await bcrypt.hash('admin', SALTROUND),
-      userType: UserType.ADMIN,
-    },
-  });
-
-  //Create regular users
-  const regularUser1 = await prisma.user.create({
-    data: {
-      email: faker.internet.email(),
-      username: faker.internet.username(),
-      password_hash: await bcrypt.hash('demouser', SALTROUND),
-      userType: UserType.USER,
-    },
-  });
-  const regularUser2 = await prisma.user.create({
-    data: {
-      email: faker.internet.email(),
-      username: faker.internet.username(),
-      password_hash: await bcrypt.hash('demouser', SALTROUND),
-      userType: UserType.USER,
-    },
-  });
-
-  const regularUser3 = await prisma.user.create({
-    data: {
-      email: faker.internet.email(),
-      username: faker.internet.username(),
-      password_hash: await bcrypt.hash('demouser', SALTROUND),
-      userType: UserType.USER,
-    },
-  });
-
-  const task1 = await prisma.task.create({
-    data: {
-      title: faker.lorem.sentence(),
-      description: faker.lorem.paragraph(),
-      creatorId: adminUser.id,
-      status: TaskStatus.OPEN,
-      budget: 100.0,
-      members: {
-        create: [{ memberId: regularUser1.id }, { memberId: regularUser2.id }],
+  const [adminUser, _superUser] = await Promise.all([
+    prisma.user.create({
+      data: {
+        email: 'admin@example.com',
+        username: 'adminUser',
+        password_hash: await bcrypt.hash('admin', SALTROUND),
+        userType: UserType.ADMIN,
       },
-    },
-  });
-
-  const task2 = await prisma.task.create({
-    data: {
-      title: faker.lorem.sentence(),
-      description: faker.lorem.paragraph(),
-      creatorId: adminUser.id,
-      status: TaskStatus.IN_PROGRESS,
-      budget: 50.0,
-      members: {
-        create: [{ memberId: regularUser2.id }, { memberId: regularUser3.id }],
+    }),
+    prisma.user.create({
+      data: {
+        email: 'super@example.com',
+        username: 'superUser',
+        password_hash: await bcrypt.hash('super', SALTROUND),
+        userType: UserType.SUPER,
       },
-    },
-  });
+    }),
+  ]);
+
+  const [user1, user2, user3] = await Promise.all([
+    prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        username: faker.internet.username(),
+        password_hash: await bcrypt.hash('demouser', SALTROUND),
+        userType: UserType.USER,
+      },
+    }),
+    prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        username: faker.internet.username(),
+        password_hash: await bcrypt.hash('demouser', SALTROUND),
+        userType: UserType.USER,
+      },
+    }),
+    prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        username: faker.internet.username(),
+        password_hash: await bcrypt.hash('demouser', SALTROUND),
+        userType: UserType.USER,
+      },
+    }),
+  ]);
+
+  const [task1, task2] = await Promise.all([
+    prisma.task.create({
+      data: {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.paragraph(),
+        creatorId: adminUser.id,
+        status: TaskStatus.OPEN,
+        budget: 100.0,
+        members: {
+          create: [{ memberId: user1.id }, { memberId: user2.id }],
+        },
+      },
+    }),
+    prisma.task.create({
+      data: {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.paragraph(),
+        creatorId: adminUser.id,
+        status: TaskStatus.IN_PROGRESS,
+        budget: 50.0,
+        members: {
+          create: [{ memberId: user2.id }, { memberId: user3.id }],
+        },
+      },
+    }),
+  ]);
 
   // Create expenses
-  await prisma.expense.create({
-    data: {
-      description: faker.lorem.words(),
-      amount: 30.0,
-      taskId: task1.id,
-      contributorId: regularUser1.id,
-    },
-  });
-
-  await prisma.expense.create({
-    data: {
-      description: faker.lorem.words(),
-      amount: 70.0,
-      taskId: task2.id,
-      contributorId: regularUser2.id,
-    },
-  });
+  await Promise.all([
+    prisma.expense.create({
+      data: {
+        description: faker.lorem.words(),
+        amount: 30.0,
+        taskId: task1.id,
+        contributorId: user1.id,
+      },
+    }),
+    prisma.expense.create({
+      data: {
+        description: faker.lorem.words(),
+        amount: 70.0,
+        taskId: task2.id,
+        contributorId: user2.id,
+      },
+    }),
+  ]);
 }
 
 if (process.env.NODE_ENV === 'development') {
